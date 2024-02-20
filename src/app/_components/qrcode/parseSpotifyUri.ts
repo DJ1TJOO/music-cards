80059dfd8c56e7ef518cec168a59e1bc73573a21
Thomas B <t.ferb1@gmail.com>
2024-02-20 23:02:06 +0100

    feat: more logs

--- src/app/_components/qrcode/parseSpotifyUri.ts
+++ src/app/_components/qrcode/parseSpotifyUri.ts
@@ -4,6 +4,8 @@ import { formatURI } from "spotify-uri";
 
 export async function parseSpotifyUri(url: string) {
 	try {
+		console.log("parseSpotifyUri", url, formatURI(url));
+
 		return formatURI(url);
 	} catch (error) {
 		return "";
