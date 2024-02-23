b29145eb267d58db87f931ad259cdcf69e1227a7
Thomas B <t.ferb1@gmail.com>
2024-02-23 16:46:36 +0100

    testing: readded translate to print

--- src/app/generate/_components/Print.tsx
+++ src/app/generate/_components/Print.tsx
@@ -6,6 +6,15 @@ import domToImage from "dom-to-image";
 import jsPDF from "jspdf";
 import React from "react";
 
+function downloadURI(uri: string, name: string) {
+	var link = document.createElement("a");
+	link.download = name;
+	link.href = uri;
+	document.body.appendChild(link);
+	link.click();
+	document.body.removeChild(link);
+}
+
 export default function Print({
 	tracks,
 	pattern,
@@ -37,14 +46,7 @@ export default function Print({
 				},
 			})
 			.then((imgData) => {
-				if (window) {
-					var image = new Image();
-					image.src = imgData;
-
-					var w = window.open("");
-					if (w) w.document.write(image.outerHTML);
-				}
-
+				downloadURI(imgData, "music-cards.png");
 				const pdf = new jsPDF({
 					unit: "px",
 					format: "a4",
