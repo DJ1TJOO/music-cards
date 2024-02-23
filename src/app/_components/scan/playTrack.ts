ddaf5691564202c15e5069c5136da47763f9a5fb
Thomas B <t.ferb1@gmail.com>
2024-02-23 17:53:11 +0100

    feat: added qr code ignore to reader

--- src/app/_components/scan/playTrack.ts
+++ src/app/_components/scan/playTrack.ts
@@ -4,6 +4,18 @@ import { SpotifyTypes } from "@/spotify-uri/types-enum";
 import { revalidateTag } from "next/cache";
 import { cookies } from "next/headers";
 
+export function isContextUri(uri: string) {
+	return (
+		uri.includes(SpotifyTypes.Album) ||
+		uri.includes(SpotifyTypes.Playlist) ||
+		uri.includes(SpotifyTypes.Artist)
+	);
+}
+
+export function isUri(uri: string) {
+	return uri.includes(SpotifyTypes.Track);
+}
+
 async function playUri(uri: string) {
 	const accessToken = cookies().get("music_cards_token")?.value;
 
@@ -11,15 +23,13 @@ async function playUri(uri: string) {
 		throw new Error("No access token found");
 	}
 
-	const isContextUri =
-		uri.includes(SpotifyTypes.Album) ||
-		uri.includes(SpotifyTypes.Playlist) ||
-		uri.includes(SpotifyTypes.Artist);
-	const isUri = uri.includes(SpotifyTypes.Track);
+	if (!isContextUri(uri) && !isUri(uri)) {
+		throw new Error("Invalid URI");
+	}
 
 	await startPlayback(accessToken, {
-		context_uri: isContextUri ? uri : undefined,
-		uris: isUri ? [uri] : undefined,
+		context_uri: isContextUri(uri) ? uri : undefined,
+		uris: isUri(uri) ? [uri] : undefined,
 	});
 
 	revalidateTag("playback-status");
